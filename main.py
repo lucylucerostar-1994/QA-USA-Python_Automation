8ce0ff11a5c8c0cd67e861c9a4575d580d61c255
Lucero Mendoza Suarez <Lucylucerostar.1994@gmail.com>
2025-06-09 18:11:33 -0700

    helpers.py, data.py, main.py

--- main.py
+++ main.py
@@ -1,2 +1,48 @@
 
 class TestUrbanRoutes:
+    import logging
+    from selenium import webdriver
+    from pages import UrbanRoutesPage
+    from data import (
+        logging.basicConfig(level=logging.INFO)
+
+    class TestUrbanRoutes:
+
+        @classmethod
+        def setup_class(cls):
+            if not helpers.is_url_reachable(URBAN_ROUTES_URL):
+                raise Exception("Server is not reachable. Aborting tests.")
+            from selenium.webdriver.chrome.options import Options
+            chrome_options = Options()
+            chrome_options.set_capability("goog:loggingPrefs", {'performance': 'ALL'})
+            cls.driver = webdriver.Chrome(options=chrome_options)
+
+        def test_set_route(self):
+            pass
+
+        def test_select_plan(self):
+            pass
+
+        def test_fill_phone_number(self):
+            pass
+
+        def test_fill_card(self):
+            pass
+
+        def test_comment_for_driver(self):
+            pass
+
+        def test_order_blanket_and_handkerchiefs(self):
+            pass
+
+        def test_order_2_ice_creams(self):
+            for i in range(2):
+                pass
+
+        def test_car_search_model_appears(self):
+            pass
+
+        @classmethod
+        def teardown_class(cls):
+            logging.info("Closing the browser.")
+            cls.driver.quit()
