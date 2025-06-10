09713c9b31d89f7f7a8182f99935e7227c45cb17
Lucero Mendoza Suarez <Lucylucerostar.1994@gmail.com>
2025-06-09 18:11:33 -0700

    helpers.py, data.py, main.py

--- data.py
+++ data.py
@@ -1,1 +1,10 @@
-# File with constants
+# File # data.py
+#
+# URBAN_ROUTES_URL = "https://cnt-a3791ae7-c568-4804-8a27-05c5d72d9838.containerhub.tripleten-services.com"
+#
+# ADDRESS_FROM = 'East 2nd Street, 601'
+# ADDRESS_TO = '1300 1st St'
+# PHONE_NUMBER = '+1 123 123 12 12'
+# CARD_NUMBER = '1234 5678 9100'
+# CARD_CODE = '1111'
+# MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'with constants
