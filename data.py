9b2fa433bffcf1502872e62e5ddb5389d1fb58e8
Lucero Mendoza Suarez <Lucylucerostar.1994@gmail.com>
2025-06-16 18:28:34 -0700

    helpers.py, data.py, main.py

--- data.py
+++ data.py
@@ -1,11 +1,11 @@
 # data.py
 
-URBAN_ROUTES_URL =
+URBAN_ROUTES_URL = ' https://cnt-3a195128-14e6-40a6-953e-5ae76155d9be.containerhub.tripleten-services.com'
 
-ADDRESS_FROM =
-ADDRESS_TO =
-PHONE_NUMBER =
-CARD_NUMBER =
-CARD_CODE =
-MESSAGE_FOR_DRIVER =
+ADDRESS_FROM = 	'East 2nd Street, 601'
+ADDRESS_TO = 	'1300 1st St'
+PHONE_NUMBER = 	'+1 123 123 12 12'
+CARD_NUMBER = 	'1234 5678 9100'
+CARD_CODE = 	'1111'
+MESSAGE_FOR_DRIVER = 	'Stop at the juice bar, please'
 
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import logging
 from selenium import webdriver
-from pages import UrbanRoutesPage
-from data import (
-logging.basicConfig(level=logging.INFO)
+
+import data
+import helpers
 
 class TestUrbanRoutes:
 
     @classmethod
     def setup_class(cls):
-        if not helpers.is_url_reachable(URBAN_ROUTES_URL):
+        if not helpers.is_url_reachable(data.URBAN_ROUTES_URL):
             raise Exception("Server is not reachable. Aborting tests.")
         from selenium.webdriver.chrome.options import Options
         chrome_options = Options()
@@ -17,33 +17,43 @@ class TestUrbanRoutes:
 
     def test_set_route(self):
         # Add in S8
+        print("Test set route")
         pass
 
     def test_select_plan(self):
-         # Add in S8
+        # Add in S8
+        print("Test select plan")
         pass
 
     def test_fill_phone_number(self):
-         # Add in S8
+        # Add in S8
+        print("Test phone number")
         pass
 
     def test_fill_card(self):
-         # Add in S8
+        # Add in S8
+        print("Test fill card")
         pass
 
     def test_comment_for_driver(self):
-         # Add in S8
+        # Add in S8
+        print("Test for driver")
         pass
 
     def test_order_blanket_and_handkerchiefs(self):
-         # Add in S8
+        # Add in S8
+        print("Test order blanket and handkerchiefs")
         pass
 
     def test_order_2_ice_creams(self):
+        # Add in S8
+        print("Test order 2 ice creams")
         for i in range(2):
             pass
 
     def test_car_search_model_appears(self):
+        # Add in S8
+        print("Test search model")
         pass
 
     @classmethod
