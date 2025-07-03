e5af585bfa913096dbc84376c53820888f69605a
Lucero Mendoza Suarez <Lucylucerostar.1994@gmail.com>
2025-07-02 19:57:54 -0700

    helpers.py, data.py, main.py

--- helpers.py
+++ helpers.py
@@ -16,16 +16,21 @@ def retrieve_phone_code(driver) -> str:
                     and 'api/v1/number?number' in log.get("message")]
             for log in reversed(logs):
                 message_data = json.loads(log)["message"]
-                body = driver.execute_cdp_cmd('Network.getResponseBody',
-                                              {'requestId': message_data["params"]["requestId"]})
+                body = driver.execute_cdp_cmd(
+                    'Network.getResponseBody',
+                    {'requestId': message_data["params"]["requestId"]}
+                )
                 code = ''.join([x for x in body['body'] if x.isdigit()])
         except WebDriverException:
             time.sleep(1)
             continue
-        if not code:
-            raise Exception("No phone confirmation code found.\n"
-                            "Please use retrieve_phone_code only after the code was requested in your application.")
-        return code
+    if not code:
+        raise Exception(
+            "No phone confirmation code found.\n"
+            "Please use retrieve_phone_code only after the code was requested in your application."
+        )
+    return code
+
 
 # Checks if Routes is up and running. Do not change
 def is_url_reachable(url):
@@ -38,3 +43,8 @@ def is_url_reachable(url):
     try:
         ssl_ctx = ssl.create_default_context()
         ssl_ctx.check_hostname = False
+        with urllib.request.urlopen(url, context=ssl_ctx, timeout=5) as response:
+            return response.status == 200
+    except Exception as e:
+        print(e)
+        return False
